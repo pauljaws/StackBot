f11f15cf6c48f2b812e5b971f7752c6fef09a167
pauljaws <paul.m.jaworski@gmail.com>
2018-03-24 13:14:47 -0700

    setup for heroku

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const express = require('express');
 const bodyParser = require('body-parser');
 require('dotenv').config();
 
-const app = express();
 const VERIFY_TOKEN = process.env.FB_VERIFY_TOKEN;
+const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
