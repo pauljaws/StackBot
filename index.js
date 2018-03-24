ca13657baf9d5aceb5be497f9362805c33ad4523
pauljaws <paul.m.jaworski@gmail.com>
2018-03-24 12:53:20 -0700

    add eslint and clean up

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const request = require('request');
+
 const app = express();
 const VERIFY_TOKEN = 'ZgpZ8fBQWZSW7gyuHJbhfCNpqr1Sw5IN';
 
@@ -9,22 +9,18 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 // Adds support for GET requests to our webhook
 app.get('/webhook', (req, res) => {
-
   // Parse the query params
-  let mode = req.query['hub.mode'];
-  let token = req.query['hub.verify_token'];
-  let challenge = req.query['hub.challenge'];
+  const mode = req.query['hub.mode'];
+  const token = req.query['hub.verify_token'];
+  const challenge = req.query['hub.challenge'];
 
   // Checks if a token and mode is in the query string of the request
   if (mode && token) {
-
     // Checks the mode and token sent is correct
     if (mode === 'subscribe' && token === VERIFY_TOKEN) {
-
       // Responds with the challenge token from the request
       console.log('WEBHOOK_VERIFIED');
       res.status(200).send(challenge);
-
     } else {
       // Responds with '403 Forbidden' if verify tokens do not match
       res.sendStatus(403);
@@ -34,19 +30,16 @@ app.get('/webhook', (req, res) => {
 
 // Creates the endpoint for our webhook
 app.post('/webhook', (req, res) => {
-
-  let body = req.body;
+  const { body } = req;
 
   // Checks this is an event from a page subscription
   if (body.object === 'page') {
-
     // Iterates over each entry - there may be multiple if batched
-    body.entry.forEach(function(entry) {
-
+    body.entry.forEach((entry) => {
       // Gets the message. entry.messaging is an array, but
       // will only ever contain one message, so we get index 0
-      let webhook_event = entry.messaging[0];
-      console.log(webhook_event);
+      const webhookEvent = entry.messaging[0];
+      console.log(webhookEvent);
     });
 
     // Returns a '200 OK' response to all requests
@@ -55,7 +48,6 @@ app.post('/webhook', (req, res) => {
     // Returns a '404 Not Found' if event is not from a page subscription
     res.sendStatus(404);
   }
-
 });
 
 const server = app.listen(process.env.PORT || 5000, () => {
