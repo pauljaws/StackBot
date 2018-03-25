ef46e2a81869b2afde7099d5a05e57d5e3c2bda5
pauljaws <paul.m.jaworski@gmail.com>
2018-03-24 18:24:24 -0700

    handle messages and respond

--- index.js
+++ index.js
@@ -1,12 +1,60 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const request = require('request');
 require('dotenv').config();
 
-const VERIFY_TOKEN = process.env.FB_VERIFY_TOKEN;
+const VERIFY_TOKEN = process.env.FB_VERIFY_TOKEN || '';
+const PAGE_TOKEN = process.env.FB_PAGE_TOKEN || '';
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+// Sends response messages via the Send API
+function callSendAPI(senderPsid, response) {
+  // Construct the message body
+  const reqBody = {
+    recipient: {
+      id: senderPsid,
+    },
+    message: response,
+  };
+
+  // Send the HTTP request to the Messenger Platform
+  request({
+    uri: 'https://graph.facebook.com/v2.6/me/messages',
+    qs: { access_token: PAGE_TOKEN },
+    method: 'POST',
+    json: reqBody,
+  }, (err) => {
+    if (!err) {
+      console.log('message sent!');
+    } else {
+      console.error(`Unable to send message: ${err}`);
+    }
+  });
+}
+
+// Handles messages events
+function handleMessage(senderPsid, receivedMessage) {
+  let response;
+
+  // Check if the message contains text
+  if (receivedMessage.text) {
+    // Create the payload for a basic text message
+    response = {
+      text: `Message received: "${receivedMessage.text}".`,
+    };
+  }
+
+  // Sends the response message
+  callSendAPI(senderPsid, response);
+}
+
+// Handles messaging_postbacks events
+function handlePostback(senderPsid, receivedPostback) {
+
+}
 
 // Adds support for GET requests to our webhook
 app.get('/webhook', (req, res) => {
@@ -41,6 +89,15 @@ app.post('/webhook', (req, res) => {
       // will only ever contain one message, so we get index 0
       const webhookEvent = entry.messaging[0];
       console.log(webhookEvent);
+      // Get the sender PSID
+      const senderPsid = webhookEvent.sender.id;
+      console.log(`Sender PSID: ${senderPsid}`);
+      // Check event type and pass to appropriate handler
+      if (webhookEvent.message) {
+        handleMessage(senderPsid, webhookEvent.message);
+      } else if (webhookEvent.postback) {
+        handlePostback(senderPsid, webhookEvent.postback);
+      }
     });
 
     // Returns a '200 OK' response to all requests
