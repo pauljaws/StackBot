4362e54051b6c392e62e70eaf72a03191c90b183
pauljaws <paul.m.jaworski@gmail.com>
2018-03-26 13:26:24 -0700

    remove Messenger code

--- index.js
+++ index.js
@@ -2,72 +2,17 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongodb = require('mongodb');
 const request = require('request');
-const apiai = require('apiai');
 const slugify = require('slugify');
 require('dotenv').config();
 
 const {
-  VERIFY_TOKEN, PAGE_TOKEN, DF_ACCESS_TOKEN, SS_ACCESS_TOKEN, MONGODB_URI,
+  SS_ACCESS_TOKEN, MONGODB_URI,
 } = process.env;
-const apiaiApp = apiai(DF_ACCESS_TOKEN);
 const app = express();
 const genericError = new Error('Sorry I couldn\'t find that.');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
-// Sends response messages via the Send API to FB Messenger
-function sendToMessenger(senderPsid, response) {
-  // Construct the message body
-  const reqBody = {
-    recipient: {
-      id: senderPsid,
-    },
-    message: {
-      text: response,
-    },
-  };
-
-  // Send the HTTP request to the Messenger Platform
-  request({
-    uri: 'https://graph.facebook.com/v2.6/me/messages',
-    qs: { access_token: PAGE_TOKEN },
-    method: 'POST',
-    json: reqBody,
-  }, (err, res) => {
-    if (!err) {
-      console.log('Message sent!');
-      console.log(reqBody);
-      console.log(res);
-    } else {
-      console.error(`Unable to send message: ${err}`);
-    }
-  });
-}
-
-// Handles messages events
-function handleMessage(senderPsid, receivedMessage) {
-  // Check if the message contains text
-  if (receivedMessage.text) {
-    // Send to Dialogflow for handling
-    apiaiApp.textRequest(receivedMessage.text, { sessionId: senderPsid })
-      .on('response', (response) => {
-        console.log('Response received from Dialogflow');
-        console.log(response);
-        sendToMessenger(senderPsid, response.result.fulfillment.speech);
-      })
-      .on('error', (error) => {
-        console.error(error);
-      })
-      .end();
-  } else if (receivedMessage.attachments) {
-    const response = {
-      text: 'Sorry, I\'m not smart enough to respond to attachments yet.',
-    };
-    // Sends the response message
-    sendToMessenger(senderPsid, response);
-  }
-}
 
 // Sort StackShare API function results by popularity
 function sortByPopularity(toolTypes) {
@@ -145,56 +90,6 @@ function formatToolTypeMsg(data) {
           ${data.company_stack_count} companies are using it in their stack.\n
           Here's what people are saying about it: "${data.reasons[0].one_liner}"`;
 }
-
-// Adds support for GET requests to our webhook
-app.get('/webhook', (req, res) => {
-  // Parse the query params
-  const mode = req.query['hub.mode'];
-  const token = req.query['hub.verify_token'];
-  const challenge = req.query['hub.challenge'];
-
-  // Checks if a token and mode is in the query string of the request
-  if (mode && token) {
-    // Checks the mode and token sent is correct
-    if (mode === 'subscribe' && token === VERIFY_TOKEN) {
-      // Responds with the challenge token from the request
-      console.log('WEBHOOK_VERIFIED');
-      res.status(200).send(challenge);
-    } else {
-      // Responds with '403 Forbidden' if verify tokens do not match
-      res.sendStatus(403);
-    }
-  }
-});
-
-// Receive messages from FB Messenger
-app.post('/webhook', (req, res) => {
-  const { body } = req;
-
-  // Checks this is an event from a page subscription
-  if (body.object === 'page') {
-    // Iterates over each entry - there may be multiple if batched
-    body.entry.forEach((entry) => {
-      // Gets the message. entry.messaging is an array, but
-      // will only ever contain one message, so we get index 0
-      const webhookEvent = entry.messaging[0];
-      console.log(webhookEvent);
-      // Get the sender PSID
-      const senderPsid = webhookEvent.sender.id;
-      console.log(`Sender PSID: ${senderPsid}`);
-      // Check event type and pass to appropriate handler
-      if (webhookEvent.message) {
-        handleMessage(senderPsid, webhookEvent.message);
-      }
-    });
-
-    // Returns a '200 OK' response to all requests
-    res.status(200).send('EVENT_RECEIVED');
-  } else {
-    // Returns a '404 Not Found' if event is not from a page subscription
-    res.sendStatus(404);
-  }
-});
 
 // Fulfillment webhook for Dialogflow
 app.post('/dialogflow', (req, res) => {
